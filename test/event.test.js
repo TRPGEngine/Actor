e66695e311a59372d93b2b98b1c25013389a7eb8
moonrailgun <moonrailgun@gmail.com>
2019-02-26 15:32:31 +0800

    :fire: getTemplate test case

---
+++ test/event.test.js
@@ -0,0 +1,43 @@
+const db = global.db;
+const emitEvent = global.emitEvent;
+
+beforeAll(async () => {
+  const loginInfo = await emitEvent('player::login', {
+    username: 'admin1',
+    password: '21232f297a57a5a743894a0e4a801fc3'
+  })
+  expect(loginInfo.result).toBe(true);
+  this.userInfo = loginInfo.info;
+})
+
+afterAll(async () => {
+  let {
+    uuid,
+    token
+  } = this.userInfo;
+  await emitEvent('player::logout', { uuid, token })
+})
+
+describe('template event', () => {
+  beforeAll(async () => {
+    expect(db.models).toHaveProperty('actor_template');
+    this.testTemplate = await db.models.actor_template.findOne();
+  })
+
+  test('getTemplate all should be ok', async () => {
+    let ret = await emitEvent('actor::getTemplate');
+    expect(ret.result).toBe(true);
+    expect(ret).toHaveProperty('templates');
+  })
+
+  test('getTemplate specify should be ok', async () => {
+    let ret = await emitEvent('actor::getTemplate', {uuid: this.testTemplate.uuid});
+    expect(ret.result).toBe(true);
+    expect(ret).toHaveProperty('template');
+    expect(ret.template.uuid).toBe(this.testTemplate.uuid);
+  })
+})
+
+describe('actor event', () => {
+
+})
